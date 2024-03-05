379bce5074b7e913cb0feebc8730cc62b7017992
Alex Rudy <github@alexrudy.net>
2024-03-05 22:07:58 +0000

    Remove assets from git repo

--- setup.py
+++ setup.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 """The setup script."""
 from setuptools import setup
+from setuptools.command.sdist import sdist
 
 
-setup()
+class WebpackedSdist(sdist):
+    def run(self) -> None:
+        from subprocess import run
+
+        run(["npm", "ci"], check=True)
+        run(["npm", "run", "build"], check=True)
+        super().run()
+
+
+setup(cmdclass={"sdist": WebpackedSdist})

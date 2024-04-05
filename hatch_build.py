ba41149940b0692b55cc6f57cb90589f53d2b9c6
Alex Rudy <github@alexrudy.net>
2024-04-05 16:47:26 +0000

    Tweak build process

--- hatch_build.py
+++ hatch_build.py
@@ -14,26 +14,37 @@ class WebpackBuildHook(BuildHookInterface):
             kwargs.setdefault("capture_output", True)
         subprocess.run(command, **kwargs)
 
+    def _assets(self) -> str:
+        if (assets := self.config.get("assets")) is not None:
+            return assets
+        self.app.display_error("missing assets configuration")
+        self.app.abort("assets configuration is required")
+        raise AssertionError("unreachable code")
+
     def clean(self, versions: list[str]) -> None:
-        self.check_command(["rm", "-rf", "src/basingse/assets/*"])
+
+        if assets := self.config.get("assets") is not None:
+            self.check_command(["rm", "-rf", f"{assets}/*"])
 
     def build_assets(self, version: str, build_data: dict[str, Any]) -> None:
-
+        assets = self._assets()
         package = os.path.join(self.root, "package.json")
         if not os.path.exists(package):
             self.app.display_error(f"expected {package}")
             self.app.abort("package.json not found, package is mis-configured")
+
+        build_data.get("artifacts", []).append(f"{assets}/*")
+        if os.path.exists(os.path.join(assets, "manifest.json")):
+            self.app.display_info("found existing manifest.json")
+            return
 
         self.app.display_waiting("building webpack bundled assets...")
         self.check_command(["npm", "ci"])
         self.check_command(["npm", "run", "build"])
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
-        assets: str | None
-        if (assets := self.config.get("assets")) is None:
-            self.app.display_error("missing assets configuration")
-            self.app.abort("assets configuration is required")
-            raise AssertionError("unreachable code")
+
+        assets = self._assets()
 
         manifest = os.path.join(self.root, assets, "manifest.json")
         if os.path.exists(manifest) and os.path.isfile(manifest):

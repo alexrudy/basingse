6f8389ef53c0af2994128d829f658681e7abba2b
Alex Rudy <github@alexrudy.net>
2025-02-26 23:52:19 +0000

    Improve test coverage of autoimport

--- src/basingse/autoimport.py
+++ src/basingse/autoimport.py
@@ -9,6 +9,8 @@ from werkzeug.utils import import_string
 
 logger = structlog.get_logger(__name__)
 
+DEFAULT_AVOID = {"tests", "test", "testing", "wsgi", "app"}
+
 
 @dc.dataclass()
 class AutoImport:
@@ -20,32 +22,32 @@ class AutoImport:
         name = self.name or app.import_name
         self.auto_import(app, name, self.avoid)
 
-    def auto_import(self, app: Flask, name: str, avoid: None | Iterable[str] = None) -> None:
+    def auto_import(self, app: Flask, name: str, avoid: None | Iterable[str] = None) -> "AutoImportRecord":
 
         # Truncate .app if we are in a .app module (not package) so that users can pass __name__
-        if name.endswith(".app") and __file__.endswith("app.py"):
+        if name.endswith(".app"):
             name = name[:-4]
 
-        avoid = {"tests", "test", "testing", "wsgi", "app"} if avoid is None else set(avoid)
+        avoid = DEFAULT_AVOID if avoid is None else set(avoid)
 
         records = []
 
-        for module in find_modules(name, include_packages=True, recursive=True):
+        for module_name in find_modules(name, include_packages=True, recursive=True):
 
-            if set(module.split(".")).intersection(avoid):
-                records.append(AutoImportModuleRecord(module, skipped=True, initialized=False))
+            if set(module_name.split(".")).intersection(avoid):
+                records.append(AutoImportModuleRecord(module_name, skipped=True, initialized=False))
                 continue
 
-            module = import_string(module)
+            module = import_string(module_name)
             if hasattr(module, "init_app"):
                 module.init_app(app)
-                records.append(AutoImportModuleRecord(module, skipped=False, initialized=True))
+                records.append(AutoImportModuleRecord(module_name, skipped=False, initialized=True))
             else:
-                records.append(AutoImportModuleRecord(module, skipped=False, initialized=False))
+                records.append(AutoImportModuleRecord(module_name, skipped=False, initialized=False))
 
         record = AutoImportRecord(name, avoid, records)
-        logger.debug("AutoImport finished", record=record)
         app.extensions["autoimport"] = record
+        return record
 
 
 @dc.dataclass(frozen=True)
@@ -67,3 +69,12 @@ class AutoImportRecord:
     name: str
     avoid: set[str]
     modules: list[AutoImportModuleRecord]
+
+    def __len__(self) -> int:
+        return len(self.modules)
+
+    def __getitem__(self, name: str) -> AutoImportModuleRecord:
+        for module in self.modules:
+            if module.name == name:
+                return module
+        raise KeyError(f"Module {name} not found in {self.name}")
---
+++ tests/test_autoimport.py
@@ -0,0 +1,147 @@
+import dataclasses as dc
+from typing import Any
+from typing import cast
+from typing import Never
+
+import pytest
+from flask import Flask
+
+from basingse.autoimport import AutoImport
+from basingse.autoimport import AutoImportModuleRecord
+from basingse.autoimport import DEFAULT_AVOID
+
+
+@dc.dataclass
+class FakeFlask:
+    import_name: str = "a_real_module.app"
+    extensions: dict[str, Any] = dc.field(default_factory=dict)
+
+
+@dc.dataclass
+class FakeModule:
+
+    initialized: bool = False
+
+
+class FakeModuleWithInit(FakeModule):
+
+    def init_app(self, app: Any) -> None:
+        self.initialized = True
+
+
+@dc.dataclass
+class FakeSetup:
+
+    modules: dict[str, FakeModule] = dc.field(default_factory=dict)
+    _calls: list[dict[str, Any]] = dc.field(default_factory=list)
+
+    def find_modules(self, name: str, include_packages: bool = False, recursive: bool = False) -> list[str]:
+        assert not name.endswith(".app")
+        self._calls.append({"name": name, "include_packages": include_packages, "recursive": recursive})
+        return list(self.modules.keys())
+
+    def import_string(self, name: str) -> FakeModule:
+        try:
+            return self.modules[name]
+        except KeyError:
+            raise ImportError(f"Module {name} not found")
+
+    def __getitem__(self, name: str) -> FakeModule:
+        return self.modules[name]
+
+
+def test_no_modules(monkeypatch: pytest.MonkeyPatch):
+
+    setup = FakeSetup()
+    monkeypatch.setattr("basingse.autoimport.find_modules", setup.find_modules)
+    monkeypatch.setattr("basingse.autoimport.import_string", setup.import_string)
+
+    app = cast(Flask, FakeFlask())
+    ai = AutoImport()
+    ai.auto_import(app, "not_a_real_module")
+
+    record = app.extensions["autoimport"]
+
+    assert record.modules == []
+    assert record.avoid == DEFAULT_AVOID
+
+
+def test_imports(monkeypatch: pytest.MonkeyPatch):
+
+    setup = FakeSetup()
+    setup.modules["simple"] = FakeModule()
+    setup.modules["with_init"] = FakeModuleWithInit()
+    monkeypatch.setattr("basingse.autoimport.find_modules", setup.find_modules)
+    monkeypatch.setattr("basingse.autoimport.import_string", setup.import_string)
+
+    app = cast(Flask, FakeFlask())
+    ai = AutoImport()
+    ai.auto_import(app, "not_a_real_module.app")
+
+    record = app.extensions["autoimport"]
+    with pytest.raises(KeyError):
+        record["not_a_real_module"]
+
+    assert not setup["simple"].initialized
+    assert setup["with_init"].initialized
+    assert len(record) == 2
+    assert record.avoid == DEFAULT_AVOID
+
+
+def test_imports_via_init_app(monkeypatch: pytest.MonkeyPatch):
+
+    setup = FakeSetup()
+    setup.modules["simple"] = FakeModule()
+    setup.modules["with_init"] = FakeModuleWithInit()
+    monkeypatch.setattr("basingse.autoimport.find_modules", setup.find_modules)
+    monkeypatch.setattr("basingse.autoimport.import_string", setup.import_string)
+
+    app = cast(Flask, FakeFlask("a_real_module.app"))
+    ai = AutoImport()
+    ai.init_app(app)
+
+    assert setup._calls[0]["name"] == "a_real_module"
+
+    record = app.extensions["autoimport"]
+    with pytest.raises(KeyError):
+        record["not_a_real_module"]
+
+    assert not setup["simple"].initialized
+    assert setup["with_init"].initialized
+    assert len(record) == 2
+    assert record.avoid == DEFAULT_AVOID
+
+
+def test_imports_avoid(monkeypatch: pytest.MonkeyPatch):
+    MODULE = "a.real.module"
+    module = FakeModule()
+
+    setup = FakeSetup()
+    setup.modules[MODULE] = module
+    monkeypatch.setattr("basingse.autoimport.find_modules", setup.find_modules)
+    monkeypatch.setattr("basingse.autoimport.import_string", setup.import_string)
+
+    app = cast(Flask, FakeFlask())
+    ai = AutoImport()
+    ai.auto_import(app, "starting.point", avoid=["real"])
+
+    record = app.extensions["autoimport"]
+
+    assert not module.initialized
+
+    assert len(record) == 1
+    assert record[MODULE].skipped, "Module should have been skipped"
+    assert record.avoid == {"real"}
+
+
+@pytest.mark.parametrize(
+    "record, expected",
+    [
+        (AutoImportModuleRecord(name="foo", skipped=False, initialized=False), "<foo imported>"),
+        (AutoImportModuleRecord(name="foo", skipped=True, initialized=False), "<foo skipped>"),
+        (AutoImportModuleRecord(name="foo", skipped=False, initialized=True), "<foo initialized>"),
+        (AutoImportModuleRecord(name="foo", skipped=True, initialized=True), "<foo skipped>"),
+    ],
+)
+def test_module_record_repr(record: AutoImportModuleRecord, expected: str):
+    assert repr(record) == expected
